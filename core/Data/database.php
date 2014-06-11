4c14792243bca864e018c02508d1739978be1019
Pranjal Pandey <pranjaltech10@gmail.com>
2014-06-12 03:42:52 +0530

    Enhancing Database codes...

--- core/Data/database.php
+++ core/Data/database.php
@@ -2,15 +2,30 @@
 namespace Core\Data
 {
 class MySQL
-{
+ {
+
+/*change this line with your local settings*/
 $host="localhost";
 $username="root";
 $password="";
-$dbname="testdb";
+/* End of change setting*/
+
+//connect to database
 function connectdb(){
 mysqli_connect($host,$username,$password,$dbname)
-  or die("Unable to connect to MySQL");
-}
+  or die("Unable to connect to MySQL". mysqli_connect_error()); }
+
+
+//creating a new DB for the socimini engine
+function createdb(){
+$sql="CREATE DATABASE sociamini";
+if (mysqli_query($this->connectdb(),$sql)) {
+  echo "Database socimini created successfully";
+} else {
+  echo "Error creating database: " . mysqli_error($con);
+} }
+
+
 }
 }
 ?>
