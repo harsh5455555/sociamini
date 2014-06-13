a80a942db0d9c302f4916c904dcb6bc738698cc7
Pranjal Pandey <pranjaltech10@gmail.com>
2014-06-13 19:06:57 +0530

    Introducing ToroPHP for URL routing

--- core/start.php
+++ core/start.php
@@ -9,9 +9,12 @@
  */
  
 // Auto loading the socia mini engine via symphony class loader
-require_once __DIR__ . '/external/Symfony/Component/ClassLoader/UniversalClassLoader.php');
+require_once __DIR__ . '/external/Symfony/Component/ClassLoader/UniversalClassLoader.php';
     $loader = new \Symfony\Component\ClassLoader\UniversalClassLoader();
     $loader->registerNamespace('Sociamini', __DIR__.'/core'); 
     $loader->register();
+	
+//Using ToroPHP for URL routing
+require_once _DIR__ . '/external/Toro/Toro.php';
 
 	?>
